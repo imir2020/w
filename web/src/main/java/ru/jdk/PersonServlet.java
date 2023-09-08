17d9eefa7cba7aedb779e920a95e7a932a37fe0f
Vladimir <imir2020@yandex.ru>
2023-09-08 16:21:34 +0300

    three

--- web/src/main/java/ru/jdk/PersonServlet.java
+++ web/src/main/java/ru/jdk/PersonServlet.java
@@ -18,18 +18,34 @@ public class PersonServlet extends HttpServlet {
         super.init();
     }
 
+    /**
+     * Добавить обработку следующих запросов в модуль web:
+     * <p>
+     * - Получить список всех пользователей
+     * <p>
+     * - Вернуть информацию о пользователе по его Id
+     * <p>
+     * - Изменить имя пользователя по его Id
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         var person = personService.getPerson(7369973181362102382L);
 
-        resp.setContentType("text/html");
+        resp.setContentType("text/html; charset=UTF-8");
         var writer = resp.getWriter();
+        writer.println("index.jsp");
         writer.println("<html><body>");
 
         writer.println("<h1> Person: </h1>");
         writer.println("<p style = 'color:Tomato'> " + person.get().getName() + "</p>");
+        writer.println("<h2> listPersons: </h2>");
+        for (Object p :personService.listAllPersons()) {
+            writer.println("<br>" + p + "</br>");
+        }
 
         writer.println("</body></html>");
+        System.out.println(resp.isCommitted());
+        writer.flush();
         writer.close();
     }
 
